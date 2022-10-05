fe4b312fd43637c30f65034d973716178193fc31
marianasaraivameli <mariana.saraiva@mercadolivre.com>
2022-10-05 16:33:43 -0300

    feat: implements the writerException class in handlerException

--- src/main/java/com/example/desafio_spring/advice/HandlerException.java
+++ src/main/java/com/example/desafio_spring/advice/HandlerException.java
@@ -1,6 +1,7 @@
 package com.example.desafio_spring.advice;
 
 import com.example.desafio_spring.advice.exception.NotFoundException;
+import com.example.desafio_spring.advice.exception.WriterValueException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -20,5 +21,16 @@ public class HandlerException {
                 .timeStamp(LocalDateTime.now())
                 .build();
         return new ResponseEntity<>(exceptionDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(WriterValueException.class)
+    public ResponseEntity<ExceptionDetails> handlerWriterValueException(WriterValueException ex) {
+        ExceptionDetails exceptionDetails = ExceptionDetails.builder()
+                .title("File not saved!")
+                .message(ex.getMessage())
+                .status(HttpStatus.NOT_ACCEPTABLE.value())
+                .timeStamp(LocalDateTime.now())
+                .build();
+        return new ResponseEntity<>(exceptionDetails, HttpStatus.NOT_ACCEPTABLE);
     }
 }
